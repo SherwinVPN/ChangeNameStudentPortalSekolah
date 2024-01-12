3edbc0d97c326cece2b503e1a3dcad8a4182ce09
SherwinVPN <sherwin.simbolon@noble-software.com>
2024-01-12 15:46:28 +0700

    coba commmit

---
+++ fileData.py
@@ -0,0 +1,15 @@
+class DataLogin():
+    loginTes = "login"
+    passwordtes = "password"
+    IdDescript = "tinymce"
+    namaguru = "Sherwin2"
+    passwordguru = "portal123"
+    namamurid = "Angela"
+    namapassowrdmurid = "portal123"
+class DataLoginAdmin():
+    namaadmin = "widayat"
+    namapasswordadmin = "admankind-09"
+
+class fieldAnnounc():
+    judul = "Pengumuman Automation"
+    deskripsi = "Pengumuman - pengumuman"
---
+++ pages.py
@@ -0,0 +1,29 @@
+class Locator():
+    locatorsubmit = '//button[@type="submit" and text() ="Masuk"]'
+    locatorassesment = '//li[@class="menu-item icon-ujian"]'
+    locatorcreateassesment = '//div[@class="profile-left"]'
+    locatorcolomassesment = '//div[@class="da-teacher-dropdown-toggle"]'
+    subject = '//div[.="Kelas 7 - Bahasa Inggris"]'
+    assesmenTitle = '//input[@id="titleIB"]'
+    repport = '//li[@class="menu-item icon-report"]'
+    skip = '//button[@class="btn btn-skip margin-top"]'
+    close = '//button[@data-action="close"]'
+    bukuNilai = '//img[@alt="Lihat Buku Nilai"]'
+    addAnnounc = '//button[@class="addAnnouncementIcon cursorPointer ml-auto"]'
+    tittleAnn = '//input[@class="nma-form form-control "]'
+    uploadFile = '//label[@class="nma-emptyUpload"]'
+    chooseSchool = '//input[@name="school-select"]'
+    createButton = '//div[@class="btn btn-primary nma-button-create ml-auto "]'
+    desCript = '//body[@class="mce-content-body "]'
+    ifrAme = '//iframe[@id="tiny-react_51824402621705041547023_ifr"]'
+
+
+class LocatorUser():
+    locatorTabUser = '//li[@class="menu-item icon-user"]'
+    locatSeeUset = '//button[@class="profile-item highlight false"]'
+    inputUser = '//input[@type="text"]'
+    buttonSearch = '//button[@class="btn btn-primary font-weight-semibold mr-2"]'
+    changeOpen = '//button[@class="btn py-1 btn-outline-primary white-bg flex-all-center font-weight-medium mr-2"]'
+    changeFirstName = '//input[@name="firstName"]'
+    buttonChange = '//button[@class="btn btn-primary ae-button-create "]'
+
